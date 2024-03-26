87ca564647f651c50cd7a86723b65f92c5791540
Alexan Lima <90114869+alexanLO@users.noreply.github.com>
2024-03-26 20:08:18 -0300

    Animações de Correndo e Parado por Código.

---
+++ scenes/player/player.gd
@@ -0,0 +1,31 @@
+extends CharacterBody2D
+class_name Player
+
+@onready var texture: Sprite2D = get_node("Texture")
+
+@export var speed: int
+const JUMP_VELOCITY = -400.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		
+	horizontal_moviment_env()
+	move_and_slide()
+	texture.animate(velocity)
+	
+func horizontal_moviment_env() -> void:
+	var direction = Input.get_axis("move_left", "move_right")
+	if direction:
+		velocity.x = direction * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+	
--- script/player/player.gd
+++ script/player/player.gd
@@ -1,34 +1,26 @@
 extends CharacterBody2D
+class_name Player
 
 
-@export var move_speed: int
-#const JUMP_VELOCITY = -400.0
+@export var speed: int
+@export var texture: PlayerTexture
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-#var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
-
+#Godot recomenda usar essa funcão para objetos que usam a física. Funções proprias da godot não precisa especificar
+#se é void ou não.
 func _physics_process(delta: float):
-	# Add the gravity.
-	#if not is_on_floor():
-		#velocity.y += gravity * delta
-	
-	# Handle jump.
-	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		#velocity.y = JUMP_VELOCITY
-		
 	horizontal_moviment_env()
 	#Esse é um metódo do KinematicBody que aplica uma velocidade linear ao objeto e apartir disso ele vai movimentar 
 	#o personagem, a vantagem é que além de movimentar o personagem, ele vai slidar. Slide faz com que ele se movimente
 	#e quando ele encontrar uma colisão dependendo dos parâmetros ele vai executar determinadas ações, se não 
 	#especificar um parâmetro quando ele encontrar uma colisão ele vai parar.
 	move_and_slide()
+	#player_texture.animate()
 	
 func horizontal_moviment_env() -> void:
 	#Aqui vai retornar qual é a direção. Se for para a esquerda é -1, se for para a direita é 1
 	#e ao pressionar os dois ou nenhum será 0.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction: float = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	if direction:
-		velocity.x = direction * move_speed
-	else:
-		velocity.x = move_toward(velocity.x, 0, move_speed)
+		velocity.x = direction * speed
+	#else:
+		#velocity.x = move_toward(velocity.x, 0, speed)
